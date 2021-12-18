0aa23e239fa94a0fe1b4e29b9c9451964378d57a
Tonny Kayage <tonnykayage@gmail.com>
2021-12-18 16:09:04 +0300

    👌 IMPROVE: Refactor to use numbers in identity types

--- src/modules/customer/enum/id-type.enum.ts
+++ src/modules/customer/enum/id-type.enum.ts
@@ -1,14 +1,9 @@
-import { registerEnumType } from "@nestjs/graphql";
+import { registerEnumType } from '@nestjs/graphql';
 
 export enum IdType {
-    NIN = "National Identification Number (NIN)",
-    VOTERS_REG_NUM = "Voters registration number",
-    PASSPORT_NUM = "Passport number",
-    DRIVING_LICENSE = "Driving License",
-    ZAN_ID = "Zanzibar Resident Id(ZANID)",
-    TIN = "Tax Identification Number (TIN)",
-    COMPANY_INC_CERT_NUMBER = "Company Incorporation Certificate Number"
+  NIN = 1,
+  VOTERS_REG_NUM = 2,
+  PASSPORT_NUM = 3,
 }
 
 registerEnumType(IdType, { name: 'IdType' });
-
