6767a41967b00315cbf85403a569b7c26cd2af45
Tonny Kayage <tonnykayage@gmail.com>
2022-01-17 12:40:47 +0300

    Change Motor cover type enum & remove category

--- src/modules/motor-cover/dtos/create-motor-cover-type.dto.ts
+++ src/modules/motor-cover/dtos/create-motor-cover-type.dto.ts
@@ -1,34 +1,30 @@
-import { Field, InputType } from "@nestjs/graphql";
-import { MotorCategory } from "../enums/motor-category.enum";
-import { MotorUsage } from "../enums/motor-usage.enum";
+import { Field, InputType } from '@nestjs/graphql';
+import { MotorCategory } from '../enums/motor-category.enum';
+import { MotorUsage, MotorUsageType } from '../enums/motor-usage.enum';
 
 @InputType()
 export class CreateMotorCoverTypeDto {
+  @Field()
+  productCode: string;
 
-    @Field()
-    productCode: string;
+  @Field()
+  productName: string;
 
-    @Field()
-    productName: string;
+  @Field()
+  riskName: string;
 
-    @Field()
-    riskName: string;
+  @Field()
+  riskCode: string;
 
-    @Field()
-    riskCode: string;
+  @Field()
+  rate: number;
 
-    @Field()
-    rate: number;
+  @Field()
+  minimumAmount: number;
 
-    @Field()
-    minimumAmount: number
+  @Field()
+  perSeatAmount: number;
 
-    @Field()
-    perSeatAmount: number;
-
-    @Field()
-    category: MotorCategory;
-
-    @Field()
-    usage: MotorUsage;
+  @Field()
+  usage: MotorUsageType;
 }
