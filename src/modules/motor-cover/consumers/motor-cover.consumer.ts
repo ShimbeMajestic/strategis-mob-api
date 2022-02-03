31b1a2175be8faab867b1172ee11afa5bead918c
Tonny Kayage <tonnykayage@gmail.com>
2022-02-03 15:53:57 +0300

    🐛 FIX: Remove POL_SRC_CODE

--- src/modules/motor-cover/consumers/motor-cover.consumer.ts
+++ src/modules/motor-cover/consumers/motor-cover.consumer.ts
@@ -371,7 +371,7 @@ export class MotorCoverConsumer {
 
         POL_ASSR_CODE: policyAssrCode,
 
-        POL_SRC_CODE: 'AG0000002',
+        POL_SRC_CODE: '',
 
         POL_ISSUE_DT: moment(request.updatedAt)
           .format('DD MMM YYYY')
